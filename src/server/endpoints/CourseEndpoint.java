37fc2f82d574287ca091e0d750cd30d7119da773
helenewekre <hewe16@student.cbs.dk>
2017-10-17 15:29:43 +0200

    Endpoint on point

--- src/server/endpoints/CourseEndpoint.java
+++ src/server/endpoints/CourseEndpoint.java
@@ -1,25 +1,25 @@
-/*
+package server.endpoints;
+
 import com.google.gson.Gson;
-import com.sun.tools.internal.ws.processor.model.Response;
 import server.dbmanager.dbmanager1;
 import server.models.Course;
-import server.Utility.Digester;
+//import server.Utility.Digester;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
+import server.dbmanager.dbmanager3;
 
 
 public class CourseEndpoint {
 
-    dbmanager3 dbmanager3 = new dbmanager3();
+    dbmanager3 dbmanager = new dbmanager3();
 
     @GET
     @Path("/Courses")
-    public Response
     public Response loadCourses (){
 
-        ArrayList<Course> courses = dbmanager3.loadCourses();
+        ArrayList<Course> courses = dbmanager.loadCourses();
         System.out.print(courses.size());
 
 
@@ -27,4 +27,4 @@ public class CourseEndpoint {
 
     }
 
-} */
+}
