29d156708dd3369d865ae18995714c1426eb53e0
Andy Tran <at@andys-mbp.hhk.dk>
2017-10-17 12:46:59 +0200

    added SuperManager

--- src/server/dbmanager/dbmanager1.java
+++ src/server/dbmanager/dbmanager1.java
@@ -1,4 +1,30 @@
 package server.dbmanager;
+import java.sql.*;
+
+import server.models.User;
+
 
 public class dbmanager1 {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/quizDB?useSSL=false";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "hello";
+    private static Connection connection = null;
+
+    public dbmanager1() {
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private static void close() {
+        try {
+            connection.close();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+
+    }
 }
