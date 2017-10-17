55f7345e7b03c33fdcc884567ffe216ee6aca33d
Christian Brandborg <christianbrandborg@Christians-MacBook-Pro.local>
2017-10-17 14:05:03 +0200

    Rettet til

--- src/server/dbmanager/dbmanager2.java
+++ src/server/dbmanager/dbmanager2.java
@@ -8,13 +8,27 @@ import java.sql.*;
 
 public class dbmanager2 {
 
-    public dbmanager2 {
-        ResetDatabase resetDatabase = new ResetDatabase();
-    }
+        private static final String URL = "jdbc:mysql://localhost:3306/quizDB?useSSL=false";
+        private static final String USERNAME = "root";
+        private static final String PASSWORD = "hello";
+        private static Connection connection = null;
 
+        public dbmanager2() {
+            try {
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            } catch (SQLException exception) {
+                exception.printStackTrace();
+            }
+        }
 
-    private DriverManager resetDatabase;
-    private Connection connection = resetDatabase.getConnection(false);
+        private static void close() {
+            try {
+                connection.close();
+            } catch (SQLException exception) {
+                exception.printStackTrace();
+            }
+
+        }
 
     public boolean createQuiz(Quiz quiz) throws IllegalArgumentException {
         try {
