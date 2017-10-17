e71e5f76be0d801940567c3d89868b7aecd456c5
helenewekre <hewe16@student.cbs.dk>
2017-10-17 15:08:46 +0200

    Messing with endpoint, not sure if any important commits

--- src/server/endpoints/CourseEndpoint.java
+++ src/server/endpoints/CourseEndpoint.java
@@ -1,4 +1,30 @@
-package server.endpoints;
+/*
+import com.google.gson.Gson;
+import com.sun.tools.internal.ws.processor.model.Response;
+import server.dbmanager.dbmanager1;
+import server.models.Course;
+import server.Utility.Digester;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+
 
 public class CourseEndpoint {
-}
+
+    dbmanager3 dbmanager3 = new dbmanager3();
+
+    @GET
+    @Path("/Courses")
+    public Response
+    public Response loadCourses (){
+
+        ArrayList<Course> courses = dbmanager3.loadCourses();
+        System.out.print(courses.size());
+
+
+        return Response.status(200).entity(new Gson().toJson(courses)).build();
+
+    }
+
+} */
