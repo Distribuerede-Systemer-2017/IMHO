f2b92d42bc0d15b2a6c3889776dc36811628d2ed
Christian Brandborg <christianbrandborg@Christians-MacBook-Pro.local>
2017-10-17 13:47:14 +0200

    Prepared statements lavet

--- src/server/dbmanager/dbmanager2.java
+++ src/server/dbmanager/dbmanager2.java
@@ -1,26 +1,69 @@
 package server.dbmanager;
 
+import server.models.Question;
 import server.models.Quiz;
+import server.resetdatabase.ResetDatabase;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLData;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class dbmanager2 {
+
+    public dbmanager2 {
+        ResetDatabase resetDatabase = new ResetDatabase();
+    }
+
+
+    private DriverManager resetDatabase;
+    private Connection connection = resetDatabase.getConnection(false);
 
     public boolean createQuiz(Quiz quiz) throws IllegalArgumentException {
         try {
             PreparedStatement createQuiz = connection
-                    .prepareStatement("INSERT INTO Quiz (String createdBy, String url, int questionCount, String quizTitle, String quizDescription, int topicId) VALUES (?,?,?,?,?,?)");
+                    .prepareStatement("INSERT INTO Quiz (String created_by, int question_count, String quiz_title, String quiz_description, int topic_d) VALUES (?,?,?,?,?)");
 
             createQuiz.setString(1, quiz.getCreatedBy());
-            createQuiz.setString(2, quiz.getUrl());
-            createQuiz.setInt(3, quiz.getQuestionCount());
-            createQuiz.setString(4, quiz.getQuizTitle());
-            createQuiz.setString(5, quiz.getQuizDescription());
-            createQuiz.setInt(6, quiz.getTopicId());
+            createQuiz.setInt(2, quiz.getQuestionCount());
+            createQuiz.setString(3, quiz.getQuizTitle());
+            createQuiz.setString(4, quiz.getQuizDescription());
+            createQuiz.setInt(5, quiz.getTopicId());
 
             int rowsAffected = createQuiz.executeUpdate();
+            if (rowsAffected == 1) {
+                return true;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean createQuestion(Question question) throws IllegalArgumentException {
+        try {
+            PreparedStatement createQuestion = connection
+                    .prepareStatement("INSERT INTO  (VARCHAR question, int quiz_id) VALUES (?, ?)");
+            createQuestion.setString(1, question.getQuestion());
+            createQuestion.setInt(2, question.getQuizIdQuiz());
+
+            int rowsAffected = createQuestion.executeUpdate();
+            if (rowsAffected == 1) {
+                return true;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean deleteQuiz(Quiz quiz) throws IllegalArgumentException {
+        try {
+            PreparedStatement deleteQuiz = connection
+                    .prepareStatement("DELETE FROM Quiz WHERE idQuiz = ?");
+
+            deleteQuiz.setInt(1, quiz.getIdQuiz());
+
+            int rowsAffected = deleteQuiz.executeUpdate();
             if (rowsAffected == 1) {
                 return true;
             }
